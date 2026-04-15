a05493f4ec92a8088f1f5c19e3feece954352508
Jasmin4242 <st160910@stud.uni-stuttgart.de>
2026-04-15 08:42:44 +0200

    trailer MPC dynamics works

--- MPC_trailer.m
+++ MPC_trailer.m
@@ -6,13 +6,13 @@ dT = 0.25;
 trailer = Trailer('trailer');
 
 % Modell auswählen
-trailer = trailer.setModel('kinematics_vw');
+% trailer = trailer.setModel('kinematics_vw');
 % trailer = trailer.setModel('kinematics_phidot');
+% x0 = [0; 0.4; 5*pi/180; 5*pi/180; 0; 0];
 % x0 = [-0.6; -0.75; 0; 0; 0; 0];
-x0 = [0; 0.4; 5*pi/180; 5*pi/180; 0; 0];
 
-% trailer = trailer.setModel('dynamics_phidot');
-% x0 = [-0.6; -0.75; 0 ;0; 0; 0];
+trailer = trailer.setModel('dynamics_phidot');
+x0 = [0; 0.4; 5*pi/180; 5*pi/180; 0; 0; 0; 0];
 
 %% initialization
 addpath(genpath(pwd));
@@ -26,7 +26,7 @@ simulator = Simulator(trailer.getModel(), dT);
 horizon = 60;
 mpcController = MPCController(trailer, dT, horizon);
 
-k_sim = 100;
+k_sim = 200;
 [x,u,cost] = simulator.sim_MPC_closedLoop(x0, k_sim,mpcController);
 
 %% postprocessing
